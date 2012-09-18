f186edeab9828232aef9068f368f24bc40165372
Steven Jabour <sjabour@summit.com>
2012-09-18 14:16:47 -0600

    Added some seed data for quicker development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Drink.create([{ title: 'Sidecar' }, { title: 'Manhattan' }, { title: 'Martini' }])
